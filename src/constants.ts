53167de6ba89611255442dd1d9dfbc90e1a262d3
JaMagik <114476897+JaMagik@users.noreply.github.com>
2025-07-20 22:03:08 +0200

    S

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 /** Jedno pole modułu w metrach */
-export const GRID = 2.8;
+export const GRID = 1;
+
 export const HALF = GRID / 2;
 
 /** Ile pól w osi (X = Y) – możesz tu zmienić zasięg całego planu */
@@ -14,6 +15,9 @@ export const CANVAS = GRID * CELLS * PPM;
 /** Granice dozwolonych współrzędnych modułów (w metrach, środek sześcianu) */
 export const MIN = HALF;
 export const MAX = GRID * CELLS - HALF;
+
+export const CANVAS_SIZE = 600;
+
 
 /** Powierzchnia całej siatki [m²] */
 export const SURFACE_M2 = Math.pow(GRID * CELLS, 2);
